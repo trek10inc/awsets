78a696debf76a33e25bdceb4f27d587ce863245b
Jeff Carter <jcarter@trek10.com>
2020-10-01 14:45:54 -0400

    fix function scope

--- arn/main.go
+++ arn/main.go
@@ -32,7 +32,7 @@ func ParseP(arn *string) Arn {
 	return Parse(*arn)
 }
 
-func InjectResourceIdAndVersion(arn Arn, resourceArnPart string) Arn {
+func injectResourceIdAndVersion(arn Arn, resourceArnPart string) Arn {
 	resourceParts := strings.SplitN(resourceArnPart, ":", 2)
 	arn.ResourceId = resourceParts[0]
 	if len(resourceParts) == 2 {
@@ -54,24 +54,24 @@ func Parse(arn string) Arn {
 
 	colonPos := strings.Index(split[5], ":")
 	slashPos := strings.Index(split[5], "/")
-	
+
 	// type of resource followed by a colon (:)
 	// example: arn:aws:logs:us-east-1:111000111000:log-group:/aws/kinesisfirehose/aws-waf-logs-us-east-1-analytics-us2:*
 	if colonPos != -1 && (slashPos == -1 || slashPos > colonPos) {
 		resourceParts := strings.SplitN(split[5], ":", 2)
 		ret.ResourceType = resourceParts[0]
-		return InjectResourceIdAndVersion(ret, resourceParts[1])
+		return injectResourceIdAndVersion(ret, resourceParts[1])
 	}
 
 	// no resource type
 	// example: arn:aws:sns:eu-west-2:111000111000:foo
 	if m := strings.Count(split[5], "/"); m == 0 {
-		return InjectResourceIdAndVersion(ret, split[5])
+		return injectResourceIdAndVersion(ret, split[5])
 	}
 
 	// type of resource followed by a slash (/)
 	// example: arn:aws:ecs:us-east-2:111000111000:task-definition/test-OHKJUeQfdbdv:1
 	resourceParts := strings.SplitN(split[5], "/", 2)
 	ret.ResourceType = resourceParts[0]
-	return InjectResourceIdAndVersion(ret, resourceParts[1])
+	return injectResourceIdAndVersion(ret, resourceParts[1])
 }
