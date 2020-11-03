09254152ae47e860b0f0f5e4548ab77452644013
Jeff Carter <jcarter@trek10.com>
2020-11-02 21:57:04 -0500

    #14 add dynamodb backups

--- lister/dynamodb_table.go
+++ lister/dynamodb_table.go
@@ -3,6 +3,8 @@ package lister
 import (
 	"fmt"
 
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/trek10inc/awsets/option"
@@ -18,11 +20,15 @@ func init() {
 }
 
 func (l AWSDynamoDBTable) Types() []resource.ResourceType {
-	return []resource.ResourceType{resource.DynamoDbTable}
+	return []resource.ResourceType{
+		resource.DynamoDbTable,
+		resource.DynamoDbBackup,
+	}
 }
 
 func (l AWSDynamoDBTable) List(cfg option.AWSetsConfig) (*resource.Group, error) {
 	svc := dynamodb.NewFromConfig(cfg.AWSCfg)
+
 	rg := resource.NewGroup()
 	err := Paginator(func(nt *string) (*string, error) {
 		res, err := svc.ListTables(cfg.Context, &dynamodb.ListTablesInput{
@@ -40,6 +46,28 @@ func (l AWSDynamoDBTable) List(cfg option.AWSetsConfig) (*resource.Group, error)
 				return nil, fmt.Errorf("failed to describe table %s: %w", *table, err)
 			}
 			r := resource.New(cfg, resource.DynamoDbTable, tableRes.Table.TableId, tableRes.Table.TableName, tableRes.Table)
+
+			err = Paginator(func(nt2 *string) (*string, error) {
+				backups, err := svc.ListBackups(cfg.Context, &dynamodb.ListBackupsInput{
+					BackupType:              types.BackupTypeFilterAll,
+					ExclusiveStartBackupArn: nt2,
+					Limit:                   aws.Int32(50),
+					TableName:               table,
+				})
+				if err != nil {
+					return nil, fmt.Errorf("failed to list backups for table %s: %w", *table, err)
+				}
+				for _, bk := range backups.BackupSummaries {
+					bkr := resource.New(cfg, resource.DynamoDbBackup, bk.BackupName, bk.BackupName, bk)
+					bkr.AddRelation(resource.DynamoDbTable, table, "")
+					rg.AddResource(bkr)
+				}
+				return backups.LastEvaluatedBackupArn, nil
+			})
+			if err != nil {
+				return nil, err
+			}
+
 			rg.AddResource(r)
 		}
 		return res.LastEvaluatedTableName, nil
